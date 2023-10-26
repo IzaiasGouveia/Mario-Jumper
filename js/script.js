60eec37738e167da11cb8f49b805842a7e0263cd
Pedro Dias <143002988+IzaiasGouveia@users.noreply.github.com>
2023-10-26 14:12:53 -0300

    Implementação de audio na ação do pulo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
+const audio = new Audio ('./assets/audio.mp3')
+const death = new Audio ('./assets/death')
+
 const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
+    audio.play()
 }
 
 const loop = setInterval(() => {
@@ -19,6 +23,7 @@ const loop = setInterval(() => {
     console.log(pipePosition)
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        death.play()
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -30,7 +35,6 @@ const loop = setInterval(() => {
         mario.style.width = '75px'
         mario.style.marginleft = '50px'
 
-        clearInterval(loop)
 
     }
     
